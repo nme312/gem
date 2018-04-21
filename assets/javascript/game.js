fb914e4c0e0a09a06d69a3abeeb371bd3ba39852
SkyNet <nme312.noreply.github.com>
2018-04-21 16:00:54 -0500

    added basic html and javascript

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+//generate random number between 19 and 120
+var targetNum = Math.floor(Math.random()+19 * 120);
+
+//generate random number between 1 and 12
+var randomNum1 = Math.floor(Math.random()+1 * 12);
+var randomNum2 = Math.floor(Math.random()+1 * 12);
+var randomNum3 = Math.floor(Math.random()+1 * 12);
+var randomNum4 = Math.floor(Math.random()+1 * 12);
+
+//create variables to store player wins and loses
+var userWins = 0;
+var userLoses = 0;
+
