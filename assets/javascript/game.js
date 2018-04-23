ad81e31a53d071e96787374b9ade07f26e7cf674
SkyNet <nme312.noreply.github.com>
2018-04-23 15:58:49 -0500

    completed game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,9 @@
 //generate random number between 19 and 120
 var targetNum;
 
+//link target number to corresponding html element
+var targetNumSpan = $("#target-number-span");
+
 //generate random number between 1 and 12
 var randomNum1;
 var randomNum2;
@@ -10,132 +13,119 @@ var randomNum4;
 //create variable to store player's total score
 var currentTot;
 
+//link current total to corresponing html element
+var currentTotSpan = $("#current-total");
+
 //create variables to store player wins and loses
 var userWins = 0;
 var userLoses = 0;
 
+//link wins and loses to corresponding html elements
+var userWinsSpan = $("#user-wins");
+var userLosesSpan = $("#user-loses");
+
+$(".gem-1").on("click", function () {
+    currentTot = currentTot + randomNum1;
+    console.log(currentTot);
+    console.log(targetNum + " target");
+
+    currentTotSpan.text(currentTot);
+    userWinsSpan.text(userWins);
+    userLosesSpan.text(userLoses);
+
+    checkForWinLoss();
+});
+
+$(".gem-2").on("click", function () {
+    currentTot = currentTot + randomNum2;
+    console.log(currentTot);
+    console.log(targetNum + " target");
+
+    currentTotSpan.text(currentTot);
+    userWinsSpan.text(userWins);
+    userLosesSpan.text(userLoses);
+
+    checkForWinLoss();
+
+});
+
+$(".gem-3").on("click", function () {
+    currentTot = currentTot + randomNum3;
+    console.log(currentTot);
+    console.log(targetNum + " target");
+
+    currentTotSpan.text(currentTot);
+    userWinsSpan.text(userWins);
+    userLosesSpan.text(userLoses);
+
+    checkForWinLoss();
+
+});
+
+$(".gem-4").on("click", function () {
+    currentTot = currentTot + randomNum4;
+    console.log(currentTot);
+    console.log(targetNum + " target");
+
+    currentTotSpan.text(currentTot);
+    userWinsSpan.text(userWins);
+    userLosesSpan.text(userLoses);
+
+    checkForWinLoss();
+});
+
 //store game components in function
 function startGame() {
-    targetNum = Math.floor(Math.random() * 120) + 19;
-    
-    randomNum1 = Math.floor(Math.random() * 12) + 1;
-    randomNum2 = Math.floor(Math.random() * 12) + 1;
-    randomNum3 = Math.floor(Math.random() * 12) + 1;
-    randomNum4 = Math.floor(Math.random() * 12) + 1;
+        targetNum = Math.floor(Math.random() * 120) + 19;
+        targetNumSpan.text(targetNum);
 
-    currentTot = 0;
-    
-    $(".gem-1").on("click", function() {
-        currentTot = currentTot + randomNum1;
-        console.log(currentTot);
+        randomNum1 = Math.floor(Math.random() * 12) + 1;
+        randomNum2 = Math.floor(Math.random() * 12) + 1;
+        randomNum3 = Math.floor(Math.random() * 12) + 1;
+        randomNum4 = Math.floor(Math.random() * 12) + 1;
+
+        currentTot = 0;
+        currentTotSpan.text(currentTot);
+
+        userWinsSpan.text(userWins);
+        userLosesSpan.text(userLoses);
+
+        console.log(randomNum1 + " num1");
+        console.log(randomNum2 + " num2");
+        console.log(randomNum3 + " num3");
+        console.log(randomNum4 + " num4");
         console.log(targetNum + " target");
+        console.log(currentTot + " current");
+    }
 
-        if(currentTot === targetNum) {
-            userWins++;
-            startGame();
-            console.log("You win");
-            console.log(userWins + " wins");
-        }
-    
-        if(currentTot > targetNum) {
-            userLoses++;
-            startGame();
-            console.log("You lose");
-            console.log(userLoses + " loses");
-        }
+    function clickGem(gemClass) {
+        $(gem).on("click", function () {
+            currentTot = currentTot + randomNum4;
+            console.log(currentTot);
+            console.log(targetNum + " target");
+        
+            currentTotSpan.text(currentTot);
+            userWinsSpan.text(userWins);
+            userLosesSpan.text(userLoses);
+        
+            checkForWinLoss();
+        });
+    }
 
-    });
-    
-    $(".gem-2").on("click", function() {
-        currentTot = currentTot + randomNum2;
-        console.log(currentTot);
-        console.log(targetNum + " target");
+function checkForWinLoss() {
+    if (currentTot === targetNum) {
+        userWins++;
+        startGame();
+        console.log("You win");
+        console.log(userWins + " wins");
+    }
 
-        if(currentTot === targetNum) {
-            userWins++;
-            startGame();
-            console.log("You win");
-            console.log(userWins + " wins");
-        }
-    
-        if(currentTot > targetNum) {
-            userLoses++;
-            startGame();
-            console.log("You lose");
-            console.log(userLoses + " loses");
-        }
-
-    });
-
-    $(".gem-3").on("click", function() {
-        currentTot = currentTot + randomNum3;
-        console.log(currentTot);
-        console.log(targetNum + " target");
-
-        if(currentTot === targetNum) {
-            userWins++;
-            startGame();
-            console.log("You win");
-            console.log(userWins + " wins");
-        }
-    
-        if(currentTot > targetNum) {
-            userLoses++;
-            startGame();
-            console.log("You lose");
-            console.log(userLoses + " loses");
-        }
-
-    });
-
-    $(".gem-4").on("click", function() {
-        currentTot = currentTot + randomNum4;
-        console.log(currentTot);
-        console.log(targetNum + " target");
-
-        if(currentTot === targetNum) {
-            userWins++;
-            startGame();
-            console.log("You win");
-            console.log(userWins + " wins");
-        }
-    
-        if(currentTot > targetNum) {
-            userLoses++;
-            startGame();
-            console.log("You lose");
-            console.log(userLoses + " loses");
-        }
-
-    });
-
-    // if(currentTot === targetNum) {
-    //     userWins++;
-    //     startGame();
-    //     console.log("You win");
-    //     console.log(userWins + " wins");
-    // }
-
-    // if(currentTot > targetNum) {
-    //     userLoses++;
-    //     startGame();
-    //     console.log("You lose");
-    //     console.log(userLoses + " loses");
-    // }
-
-    console.log(randomNum1 + " num1");
-    console.log(randomNum2 + " num2");
-    console.log(randomNum3 + " num3");
-    console.log(randomNum4 + " num4");
-    console.log(targetNum + " target");
-    
-    
-    console.log(currentTot + " current");
-
-    
-    
+    if (currentTot > targetNum) {
+        userLoses++;
+        startGame();
+        console.log("You lose");
+        console.log(userLoses + " loses");
+    }
 }
-
-
 
 startGame();
